e184dc66224d70040dd7bb4d18b6805058507965
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-16 08:52:25 +0300

    corrects conflicting keymaps

--- neovim/init.lua
+++ neovim/init.lua
@@ -116,7 +116,7 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Replace multiple words via menu
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rp", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
